ec82500692aed3bd69e6f80276ee111ee240f5f3
Karen Quiceno <ksquiceno3@misena.edu.co>
2019-12-04 11:59:24 -0500

    Se puso codigo de error debajo de boton save en BadgeNew

--- src/componentes/badgeForm.js
+++ src/componentes/badgeForm.js
@@ -80,6 +80,9 @@ class BadgeForm extends React.Component {
                     </div>
 
                     <button onClick={this.handleClick} className="btn btn-primary">Save</button>
+
+                    {this.props.error &&(<p className="text-danger">{this.props.error.message}</p>)}
+
                 </form>
             </div>
         )
