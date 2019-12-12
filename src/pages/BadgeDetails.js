7397e707f415bed1f4805b63491a4343460c56ad
Karen Quiceno <ksquiceno3@misena.edu.co>
2019-12-12 12:12:16 -0500

    Se realizo el filter para buscar personas fue el fin

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -6,7 +6,19 @@ import confLogo from '../images/platziconf-logo.svg'
 import Badge from '../componentes/badge'
 import DeleteBadgeModal from '../componentes/DeleteBadgeModal'
 
+
+function useIncreaseCount(max){
+  const [count, setCount ] = React.useState(0)
+
+  if (count > max){
+    setCount(0)
+  }
+
+  return [count, setCount ]
+}
+
 function BadgeDetails (props) {
+  const [ count, setCount ] = useIncreaseCount(4)
   const badge = props.badge
 
   return (
@@ -40,6 +52,9 @@ function BadgeDetails (props) {
             <h2>Actions</h2>
             <div>
               <div>
+                <button onClick={() => {
+                  setCount(count + 1)
+                }} className="btn btn-primary mr-4"> Increase Count: {count}</button>
                 <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`}>Edit</Link>
               </div>
               <div>
